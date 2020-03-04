ee281501b61cd4ff18b44743026a42bec3c4a936
michellemnguyen <michellemnguyen@ucsb.edu>
2020-03-03 21:00:40 -0800

    Update server and README to include options for encryption and compression

--- README.md
+++ README.md
@@ -4,15 +4,39 @@ Node.JS simple server to upload and download files
 ### Pre-requisites
 **Nothing** No NPM Module dependency, as it is written using pure Node.JS API. Only nodejs should be installed.
 
-### Start server with default port 3000
+### Start server with default port 3000, default no encryption, default no compression
 ```
 node server.js
 ```
 
-### Start server with specific port 4200
+### Start server with specific port 4200, RSA encrpytion, compression
 ```
-node server.js 4200
+node server.js 4200 rsa compress
 ```
+
+
+### Start server with specific port 4200, AES encrpyption, compression
+```
+node server.js 4200 aes compress
+```
+
+### Start server with specific port 4200, no encrpyption, compression
+```
+node server.js 4200 none compress
+```
+
+
+### Start server with specific port 4200, AES encrpytion, default no compression
+```
+node server.js 4200 aes
+```
+
+### Start server with specific port 4200, AES encrpytion, no compression
+```
+node server.js 4200 aes none
+```
+
+
 
 ### Open in browser, show download/upload UI
 ```
--- server.js
+++ server.js
@@ -3,51 +3,62 @@ const fs = require('fs');
 const path = require('path');
 
 let port = process.argv[2] || 3000;
+
+// encryption scheme being used
+// options: 'none', 'aes', 'rsa'
+// default: 'none'
+let encryption = process.argv[3] || 'none';
+
+// compression vs no compression
+// options: 'none', 'compress'
+// default: 'none'
+let compression = process.argv[4] || 'none';
+
 const httpServer = http.createServer(requestHandler);
 httpServer.listen(port, () => {console.log('server is listening on port '+ port)});
 
-function requestHandler(req, res){
-  if(req.url === '/'){
-    sendIndexHtml(res);
-  }else if( req.url === '/list'){
-    sendListOfUploadedFiles(res);
-  }else if( /\/download\/[^\/]+$/.test(req.url)){
-    sendUploadedFile(req.url, res);
-  }else if( /\/upload\/[^\/]+$/.test(req.url) ){
-    saveUploadedFile(req, res)
-  }else{
-    sendInvalidRequest(res);
-  }
+function requestHandler(req, res) {
+    if (req.url === '/') {
+        sendIndexHtml(res);
+    } else if (req.url === '/list') {
+        sendListOfUploadedFiles(res);
+    } else if ( /\/download\/[^\/]+$/.test(req.url)) {
+        sendUploadedFile(req.url, res);
+    } else if ( /\/upload\/[^\/]+$/.test(req.url) ) {
+        saveUploadedFile(req, res)
+    } else {
+        sendInvalidRequest(res);
+    }
 }
 
-function sendIndexHtml(res){
-  let indexFile = path.join(__dirname, 'index.html');
-  fs.readFile(indexFile, (err, content) => {
-    if(err){
-      res.writeHead(404, {'Content-Type': 'text'});
-      res.write('File Not Found!');
-      res.end();
-    }else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(content);
-      res.end();
-    }
-  })
+function sendIndexHtml(res) {
+    let indexFile = path.join(__dirname, 'index.html');
+    fs.readFile(indexFile, (err, content) => {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text'});
+            res.write('File Not Found!');
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(content);
+            res.end();
+        }
+    })
 }
 
 function sendListOfUploadedFiles(res){
-  let uploadDir = path.join(__dirname, 'files');
-  fs.readdir(uploadDir, (err, files) => {
-    if(err){
-      console.log(err);
-      res.writeHead(400, {'Content-Type': 'application/json'});
-      res.write(JSON.stringify(err.message));
-      res.end();
-    }else{
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.write(JSON.stringify(files));
-      res.end();
-    }
+    let uploadDir = path.join(__dirname, 'files');
+    fs.readdir(uploadDir, (err, files) => {
+        if (err) {
+            console.log(err);
+            res.writeHead(400, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(err.message));
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(files));
+            res.end();
+        }
   })
 }
 
