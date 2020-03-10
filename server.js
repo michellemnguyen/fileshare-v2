e62f0dc6d0fc0824bc59bf08c2e26cc3dc080898
michellemnguyen <michellemnguyen@ucsb.edu>
2020-03-10 09:35:17 -0700

    Change font lol

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ function downloadDecrypt(url, res){
   
 
   function uploadEncrypt(req, res){
-    console.log('saving uploaded file');
+    console.log('saving encrypted uploaded file');
     let fileName = path.basename(req.url);
     let file = path.join(__dirname, 'files', fileName)
     req.pipe(fs.createWriteStream(file));
@@ -141,7 +141,7 @@ function downloadDecrypt(url, res){
       res.write('uploaded succesfully');
       res.end();
     })
-  }
+}
   
 
 // Nodejs encryption with CTR
@@ -149,22 +149,3 @@ let algorithm = 'aes-256-cbc';
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 
-function encrypt(text, algorithm) {
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { 
-        iv: iv.toString('hex'), 
-        encryptedData: encrypted.toString('hex') 
-    };
-}
-   
-function decrypt(text, algorithm) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
-}
-   
